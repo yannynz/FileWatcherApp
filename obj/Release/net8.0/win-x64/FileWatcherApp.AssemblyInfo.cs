e67d6117624beeb18e55a70f55caab6aeb981c18
yannynz <testeyann@hotmail.com>
2025-12-20 12:45:29 -0300

    Versão 0.4.1

--- obj/Release/net8.0/win-x64/FileWatcherApp.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/FileWatcherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileWatcherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa026d4f3b28814113cdb7485d0030db6806b4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb085a5cc946b724897173e5378705866b158e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileWatcherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileWatcherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
